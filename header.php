41c9f67bafac89aa6cbbc51cc2e919d4dade73bc
Madalina Naghiu <medeline2012@gmail.com>
2019-07-17 19:05:48 +0300

    initial commit

---
+++ header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<?php wp_head(); ?>
+	</head>
+	<body>
+
+<div class="container">
+	<div class="row">
+		<!-- Logo -->
+		<div class="col-sm-3">
+			<div class="site-logo">
+				<!-- <a href="url_home">IAP Site</a> -->
+				<a href="<?php echo get_bloginfo( 'url' ); ?>" id="">
+					<?php echo get_bloginfo( 'name' ); ?>
+				</a>
+			</div>
+		</div>
+		<!-- Meniuri -->
+		<div class="col-sm-9">
+			<div class="primary-menu">
+				<?php 
+					/**
+					 * Here we need to add the primary menu.
+					 */
+					if ( has_nav_menu( 'primary' ) ) : 
+						wp_nav_menu( array(
+						 	'theme_location'  => 'primary',
+						 	'container' => 'nav'
+						) );
+					endif;
+				?>
+			</div>
+		</div>
+	</div>
+</div>
